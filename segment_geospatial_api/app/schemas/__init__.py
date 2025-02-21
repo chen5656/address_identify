fc99cc03b4a2c3dca677a530ea6724dc29b09b2b
huajunchen <35971289+chen5656@users.noreply.github.com>
2025-02-21 14:03:34 -0600

    updated html and removed unneeded files

--- segment_geospatial_api/app/schemas/__init__.py
+++
@@ -1,2 +0,0 @@
-from .health import Health
-from .predict import PredictionRequest, PredictionResults, ErrorResponse
--- segment_geospatial_api/app/schemas/health.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class Health(BaseModel):
-    name: str
-    api_version: str
